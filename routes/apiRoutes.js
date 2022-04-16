74a643bf2f10bc1f4ae0ab0c7a4d45cf33cc23f2
sauln179 <79173328+sauln179@users.noreply.github.com>
2022-04-16 12:17:36 -0700

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,23 @@
+// Dependencies
+const router = require('express').Router();
 
+const saveData = require('../db/saveData');
+
+// GET request
+router.get('/notes', function (req, res) {
+    saveData
+        .retrieveNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+});
+
+// POST request
+router.post('/notes', (req, res) => {
+    saveData
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
+});
+
+
+module.exports = router;
